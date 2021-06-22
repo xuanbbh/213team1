db82af9212a7d255378ba40d1d86f2beb556eaf6
xuanbbh <86296549+xuanbbh@users.noreply.github.com>
2021-06-22 18:51:04 +0800

    MakeCode 專案的初始檔案

---
+++ images.g.ts
@@ -0,0 +1,19 @@
+// 自動生成的代碼。請勿編輯。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// 自動生成的代碼。請勿編輯。
